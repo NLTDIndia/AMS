db07e65faf8a5e89f68e8f3de5aa776a60b51094
Divya-Mahadevan <divyam@nltechdev.com>
2019-12-03 11:37:01 +0530

    Active removed in layout,get unique id on components, field validation in asset category model.

--- AMSUtilities/Models/AssetCategoryModel.cs
+++ AMSUtilities/Models/AssetCategoryModel.cs
@@ -5,8 +5,9 @@ namespace AMSUtilities.Models
     public class AssetCategoryModel
     {
         public int ID { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
+        [Required(ErrorMessage = "Comment is required")]
         public string Comment { get; set; }
     }
 }
