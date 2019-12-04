db68e0090720be2548879098b2ae556792b59e3b
JafferHussainN <JafferHussainN@nltechdev.com>
2019-12-04 12:32:17 +0530

    Initial DBB script commit in Git

---
+++ DB Scripts/12.04.2019/AMS Employee Insert statement.sql
@@ -0,0 +1,2 @@
+INSERT INTO [dbo].[Employee] (EmployeeName,EmployeeID,EmployeeRoleID,MailID,CreatedDate,CreatedBy,ISActive,CorpId) 
+VALUES ('Jaffer Hussain N',67,1,'JafferHussainN@nltechdev.com',GETDATE(),1,1,'CORP\JHNoordeen')
